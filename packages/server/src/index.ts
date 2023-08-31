feacd51ac43042d7b8b26cc715353c6bbe295947
bgeisb <benedikt@geisberger.biz>
2023-08-31 10:30:14 +0200

    [Server] Add cors package

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { json } from 'body-parser';
+import cors from 'cors';
 import mongoose, { ConnectOptions } from 'mongoose';
 
 // Routes
 import { issuesRouter } from './routes/issues';
 
+
 const app = express();
 app.use(json());
+app.use(cors());
 app.use(issuesRouter);
 
 const mongoURI = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/issues?authSource=admin`
