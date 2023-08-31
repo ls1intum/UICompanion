e307b31a5bc24b8a731d05293742e4ea7fc7c24b
bgeisb <benedikt@geisberger.biz>
2023-08-31 15:42:26 +0200

    [Server] Add PATCH /api/issues route

--- packages/server/src/controllers/issueController.ts
+++ packages/server/src/controllers/issueController.ts
@@ -2,12 +2,14 @@ import { Request, Response } from 'express';
 import { IIssue, Issue } from '../model/Issue';
 
 export class IssueController {
+    // getIssues function for GET /api/issues
     public async getIssues(req: Request, res: Response): Promise<void> {
         const issues = await Issue.find();
 
         res.json({ issues });
     }
 
+    // createIssue function for POST /api/issues
     public async createIssue(req: Request, res: Response): Promise<void> {
         const newIssue: IIssue = new Issue(req.body);
 
@@ -18,6 +20,27 @@ export class IssueController {
         } else {
             console.log("Issue saved successfully");
             res.status(200).send(createdIssue);
+        }
+    }
+
+    // updateIssue function for PATCH /api/issues/:number
+    public async updateIssue(req: Request, res: Response): Promise<void> {
+        const issueNumber = req.params.number;
+        const updateData = req.body;
+
+        try {
+            const updatedIssue = await Issue.findOneAndUpdate({ number: issueNumber }, updateData, { new: true });
+
+            if (!updateData) {
+                console.log("Issue not found");
+                res.status(404).send();
+            } else {
+                console.log("Issue updated successfully");
+                res.status(200).send(updatedIssue);
+            }
+        } catch (error) {
+            console.error("Error updating issue:", error);
+            res.status(500).send();
         }
     }
 }
--- packages/server/src/routes/issues.ts
+++ packages/server/src/routes/issues.ts
@@ -9,4 +9,6 @@ router.get('/api/issues', issueControler.getIssues);
 
 router.post('/api/issues', issueControler.createIssue);
 
+router.patch('/api/issues/:number', issueControler.updateIssue);
+
 export { router as issuesRouter };
